283ea276faf301a214a217c6e937d62da369768b
etahinnocent360 <etahinno360@gmail.com>
2021-12-04 23:38:54 +0100

    added email services

--- js/app.js
+++ js/app.js
@@ -179,3 +179,28 @@ window.onload = load();
 // Footer Year 
 // var year = document.getElementById("year");
 // year.innerHTML = new Date().getFullYear();
+
+//formspree
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.classList.add("success")
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.classList.add("success")
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
